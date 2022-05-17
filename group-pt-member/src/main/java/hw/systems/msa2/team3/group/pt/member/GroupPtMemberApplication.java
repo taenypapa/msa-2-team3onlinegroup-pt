06809418242bbfa08c31d3558a50c7be34f8f0f3
Seongho Lee <cylsh3452@hanwha.com>
2022-05-17 13:25:12 +0900

    member, reservation 완료

--- group-pt-member/src/main/java/hw/systems/msa2/team3/group/pt/member/GroupPtMemberApplication.java
+++ group-pt-member/src/main/java/hw/systems/msa2/team3/group/pt/member/GroupPtMemberApplication.java
@@ -3,7 +3,6 @@ package hw.systems.msa2.team3.group.pt.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableCaching
